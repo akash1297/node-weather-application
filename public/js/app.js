4a9ce14f560523cee3b1ffc209f980ff5184a943
Akash_pandey <akash.zerox505@gmail.com>
2020-09-17 20:05:49 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ console.log("Client side javascript code")
 
 
 
-        fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+        fetch('/weather?address='+location).then((response)=>{
             response.json().then((data)=>{
                 if(data.error){
                     messageOne.textContent=data.error
